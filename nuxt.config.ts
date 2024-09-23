dddaa1caac78465abe8c2ef69fadd986d8880e7a
Antonio Miguel Perez Aramillo <aperezaramillo@gmail.com>
2024-09-23 11:08:24 -0600

    feat: nuxt and primevue environment configuration

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: [
+    'nuxt-primevue'
+  ],
+  css: [
+      'primevue/resources/themes/aura-light-green/theme.css'
+  ]
+})
