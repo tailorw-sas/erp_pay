4b3b72ab32d548f3f68b4487cdc01a869a2a6391
Antonio Miguel Perez Aramillo <aperezaramillo@gmail.com>
2024-09-25 15:56:49 -0600

    feat: adding transaction result page.  - layout design configurations

---
+++ layouts/default.vue
@@ -0,0 +1,63 @@
+<script setup>
+import {ref} from 'vue'
+
+const topbarRef = ref(null)
+
+</script>
+
+<template>
+  <div class="layout-container bg-home">
+    <div class="layout-content-wrapper">
+<!--      <Toolbar ref="topbarRef" class="layout-topbar">
+        <template #center>
+          <img src="/layout/logo-simple-dark.png" alt="Logo" class="topbar-logo" />
+        </template>
+      </Toolbar>-->
+      <div class="layout-content w-full">
+        <!-- <NuxtPage /> -->
+        <slot/>
+      </div>
+
+      <div class="layout-mask"/>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+html, body, #__nuxt, .layout-container {
+  height: 100%;
+  margin: 0;
+}
+
+.bg-home {
+  background-image: url('/layout/images/img-transparent.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 100%;
+  width: 100%;
+}
+
+.layout-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.layout-content-wrapper {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.layout-content {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.topbar-logo {
+  height: 40px; /* Ajusta el tamaño del logo según tu preferencia */
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,7 @@
 <script setup lang="ts">
-import '~/assets/styles.scss'
-
-import { ref } from 'vue';
-
-const items = ref([
-  {
-    label: 'Update',
-    icon: 'pi pi-refresh'
-  },
-  {
-    label: 'Delete',
-    icon: 'pi pi-times'
-  }
-])
 </script>
 <template>
   <div>
-    <Toolbar>
-      <template #start>
-        <Button icon="pi pi-plus" class="mr-2" severity="secondary" />
-        <Button icon="pi pi-print" class="mr-2" severity="secondary" />
-        <Button icon="pi pi-upload" severity="secondary" />
-      </template>
-
-      <template #center>
-        <IconField iconPosition="left">
-          <InputIcon>
-            <i class="pi pi-search" />
-          </InputIcon>
-          <InputText placeholder="Search" />
-        </IconField>
-      </template>
-
-      <template #end> <SplitButton label="Save" :model="items"></SplitButton></template>
-    </Toolbar>
+    adsadsd
   </div>
 </template>
